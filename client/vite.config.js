04fd66ae2275dd2b39dfcb91aae4737656086d33
KumarSamarth-ofc <kumarsamarth.adept@gmail.com>
2025-03-11 18:50:57 +0530

    Fix .gitignore and add Vite build output

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // This tells Vite to output build files to the "dist" folder
+  },
+});
